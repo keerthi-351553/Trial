21712244b29dd652f2756d64006bb4c6a8570046
Keerthana N <keerthana.n@team.telstra.com>
2019-03-29 12:32:45 +0530

    initial commit

---
+++ smokeTest.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "Smoke Test ------------> Success"
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const varTitle = process.env.TITLE || "Sample NodeJs";
+const varMessage = process.env.MESSAGE || "Hello world!";
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+  res.render("index", { title: varTitle, message: varMessage });
+});
+
+module.exports = router;
