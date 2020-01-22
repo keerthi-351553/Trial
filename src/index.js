74d13bee67a14f4d3fbcca240e2f251e97c1363d
root <root@10.0.1.17>
2020-01-22 11:19:30 +0000

    initial commit by Rig

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const http = require("http");
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "5555");
+const port = normalizePort(process.env.PORT || "1111");
 app.set("port", port);
 
 /**
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 
-const varTitle = process.env.TITLE || "Sample NodeJs deployed by Atlassian Blueprint";
+const varTitle = process.env.TITLE || "Sample NodeJs deployed by Digital Rig";
 const varMessage = process.env.MESSAGE || "Hello world!";
 
 /* GET home page. */
